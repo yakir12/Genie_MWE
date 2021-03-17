8dadcd31837572dccbe2477185903fe635fbfe74
Yakir <12.yakir@gmail.com>
2021-03-17 14:22:50 +0100

    a

--- leds.jl
+++ leds.jl
@@ -19,15 +19,13 @@ function star2leds(s::Star)
     striphalf = Int(s.cardinal)
     base = isodd(striphalf) ?  s.elevation - 1 : liveleds - s.elevation
     secondstrip = striphalf ≥ 3
-    extra = secondstrip ? ledsperstrip : 0
+    extra = secondstrip ? ledsperstrip - 1 : 0
     μ = base + extra
-    @show μ
-    m = secondstrip ? ledsperstrip : 0
+    m = secondstrip ? ledsperstrip - 1 : 0
     M = secondstrip ? ledsperstrip + liveleds - 1 : liveleds - 1
     i1 = max(m, μ - s.radius)
     i2 = min(M, μ + s.radius)
     indices = replace(i1:i2, centerled => centerled + ledsperstrip - 1)
-    @show indices[1]
     LED.(indices, s.r, s.g, s.b)
 end
 
