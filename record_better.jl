ecb603d5b2b37fa85e07a94f7d6a11f77f42979f
Yakir <12.yakir@gmail.com>
2021-03-21 20:43:31 +0100

    meh

--- record_better.jl
+++ record_better.jl
@@ -6,7 +6,8 @@ p = ffmpeg() do exe
     run(`$exe -y -hide_banner -loglevel error -f v4l2 -i /dev/video0 -filter_complex '[0:v]crop=in_h:in_h,fps=30,split=2[out1][out2]' -map '[out1]' $file -map '[out2]' -s $(sz)x$sz -r $fps -update 1 $IMG_FILE`, wait = false)
 end
 
-cam = VideoIO.opencamera()
+using VideoIO
+cam = VideoIO.opencamera(;transcode=false)
 img = read(cam)
 
 encoder_options = (crf=0, preset="ultrafast")
