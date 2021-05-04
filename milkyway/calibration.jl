c0cd9758eab7f07f638acb0b84f4338d708ac4f9
Yakir Gagnon <12.yakir@gmail.com>
2021-05-04 14:07:09 +0200

    half intensity

--- milkyway/calibration.jl
+++ milkyway/calibration.jl
@@ -2,9 +2,10 @@ using Serialization
 ind2elev(x) = (x - 1)/140*180
 lb = 20
 ub = 157 #166
-elev2int(x) = lb < x < ub ? round(UInt8, 255(x - lb)/(ub - lb)) : 0x00
+M = 255/3
+elev2int(x) = lb < x < ub ? round(UInt8, M*(x - lb)/(ub - lb)) : 0x00
 inds = 1:141
 ints = elev2int.(ind2elev.(inds))
-@assert Int.(extrema(filter(!iszero, ints))) == (1, 255)
+@assert Int.(extrema(filter(!iszero, ints))) == (1, M)
 
 serialize("../calibration_vector", ints)
