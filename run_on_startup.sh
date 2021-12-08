d63731e56f24dab952b756eacbd33e146b88523f
Yakir <12.yakir@gmail.com>
2021-12-08 14:40:07 +0100

    restart

--- run_on_startup.sh
+++ run_on_startup.sh
@@ -1,2 +1,2 @@
 #! /usr/bin/bash
-screen /home/pi/julia-1.7.0-rc3/bin/julia --project="/home/pi/tmp/Genie_MWE/Project.toml" /home/pi/tmp/Genie_MWE/mwe.jl
+/home/pi/julia-1.7.0-rc3/bin/julia --project="/home/pi/tmp/Genie_MWE/Project.toml" --load /home/pi/tmp/Genie_MWE/mwe.jl
